10f309835fa684d6280885b04ee052948b5b1a11
Mark Reichardt <103030472+MarkReichardt@users.noreply.github.com>
2024-02-09 22:54:07 +0200

    26.10 kodutöö

--- routes.php
+++ routes.php
@@ -3,6 +3,7 @@
 use App\Controllers\AuthController;
 use App\Controllers\PostsController;
 use App\Controllers\PublicController;
+use App\Controllers\UsersController;
 use App\Router;
 
 Router::get('/', [PublicController::class, 'index']);
@@ -22,6 +23,14 @@ Router::get('/admin/posts/show', [PostsController::class, 'show']);
 Router::get('/admin/posts/edit', [PostsController::class, 'edit']);
 Router::post('/admin/posts/edit', [PostsController::class, 'update']);
 Router::get('/admin/posts/delete', [PostsController::class, 'destroy']);
+
+Router::get('/admin/users', [UsersController::class, 'index']);
+Router::get('/admin/users/create', [UsersController::class, 'create']);
+Router::post('/admin/users', [UsersController::class, 'store']);
+Router::get('/admin/users/show', [UsersController::class, 'show']);
+Router::get('/admin/users/edit', [UsersController::class, 'edit']);
+Router::post('/admin/users/edit', [UsersController::class, 'update']);
+Router::get('/admin/users/delete', [UsersController::class, 'destroy']);
 
 
 
