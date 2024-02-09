3e8e83dc6cf9a8d87b8b9e614230f4cc9bde846d
MarkReichardt <mark.reichardt@tptlive.ee>
2024-02-09 22:27:02 +0200

    awd

--- LearnPHP/views/404.php
+++
@@ -1,3 +0,0 @@
-<?php include __DIR__ . '/../partials/header.php' ?> 
-<img src="https://http.cat/[404]">
-<?php include __DIR__ . '/../partials/footer.php' ?> 
--- LearnPHP/views/about.php
+++
@@ -1,5 +0,0 @@
-<?php include __DIR__ . '/partials/header.php'?>
-
-<?php include __DIR__ . '/partials/footer.php'?>
-
-
