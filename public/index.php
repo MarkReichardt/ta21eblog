41255d0e01d7f7e803e7805ca877046d4bf9495b
Mark Reichardt <103030472+MarkReichardt@users.noreply.github.com>
2024-03-05 20:09:37 +0200

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -1,48 +1,55 @@
 <?php
 
-use App\Exceptions\NotFoundException;
-use App\Router;
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
 
-spl_autoload_register(function ($class){
-    $class = substr($class, 4);
-    require_once __DIR__ . "/../src/$class.php";
-});
+define('LARAVEL_START', microtime(true));
 
-session_start();
+/*
+|--------------------------------------------------------------------------
+| Check If The Application Is Under Maintenance
+|--------------------------------------------------------------------------
+|
+| If the application is in maintenance / demo mode via the "down" command
+| we will load this file so that any pre-rendered content can be shown
+| instead of starting the framework, which could cause an exception.
+|
+*/
 
-require __DIR__ . '/../helpers.php';
-require __DIR__ . '/../routes.php';
-try {
-    $router = new App\Router($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
-    $match = $router->match();
-    if($match) {
-        if(is_callable($match['action'])){
-            call_user_func($match['action']);
-        } elseif(is_array($match['action']) && count($match['action']) === 2) {
-            $class = $match['action'][0];
-            $controller = new $class();
-            $method = $match['action'][1];
-            $controller->$method();
-        } else {
-            throw new Exception('invalid router action');
-        }
-    
-    } else {
-        throw new NotFoundException();
-    }
-} catch(NotFoundException $e){
-    http_response_code(404);
-    view('404');
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
 }
-// switch($_SERVER['REQUEST_URI']){
-//     case '/':
-//         $name = 'Mark';
-//         include 'views/index.php';
-//         break;
-//     case '/about':
-//         include 'views/about.php';
-//         break;
-//     default:
-//         echo 404;
-//         break;
-// }
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
+|
+*/
+
+require __DIR__.'/../vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request using
+| the application's HTTP kernel. Then, we will send the response back
+| to this client's browser, allowing them to enjoy our application.
+|
+*/
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
